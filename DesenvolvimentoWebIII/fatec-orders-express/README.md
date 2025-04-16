860230a090667f96e8f8a42ccd6a82a4ee25f50e
Matheus Gueff <gueffmatheus@gmail.com>
2025-04-15 22:06:31 -0300

    Adicionado o código de uso de ORM de DW

---
+++ DesenvolvimentoWebIII/fatec-orders-express/README.md
@@ -0,0 +1,28 @@
+# Fatec orders
+
+* Aprendendo a criar API's
+
+### Commands
+
+```
+* npm install --save sequelize-typescript sqlite3
+
+* npm install --save-dev @types/sequelize sequelize-cli
+
+* npx sequelize-cli migration:generate --name create-table-brand
+
+* npx sequelize-cli db:migrate
+```
+
+### Create Migration
+
+```
+npm run migration:create <migration name>
+```
+
+### Migrate
+
+```
+npm run migrate
+```
+
---
+++ DesenvolvimentoWebIII/fatec-orders-express/migrations/20250415234441-create-table-brand.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('marca', 
+      { 
+        id_marca: {
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+        },
+        descricao : {
+          allowNull: false,
+          type: DataTypes.STRING(100)
+        }
+      },
+
+    );
+
+    await queryInterface.createTable('produto', 
+      { 
+        id_marca: {
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+        },
+        descricao : {
+          allowNull: false,
+          type: DataTypes.STRING(100)
+        }
+      },
+
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('marca');
+  }
+};
