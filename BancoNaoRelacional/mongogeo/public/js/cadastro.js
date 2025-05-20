a16dd4bfe9427bf6e6f04c270e248b1e269120f4
Matheus Gueff <gueffmatheus@gmail.com>
2025-05-19 22:08:33 -0300

    Adicionado cadastro

--- BancoNaoRelacional/mongogeo/public/js/cadastro.js
+++ BancoNaoRelacional/mongogeo/public/js/cadastro.js
@@ -1,29 +1,29 @@
-const urlBase =  window.location.href + 'api'
+const urlBase = 'http://localhost:3000/'+ 'api'
 
 document.getElementById('formLogin').addEventListener('submit', function(event){
         event.preventDefault();
         const msgModal = new bootstrap.Modal(document.getElementById('modalMensagem'))
 
+        const nome = document.getElementById('nome').value
         const login = document.getElementById('login').value
         const senha = document.getElementById('senha').value
 
         const dadosLogin = {
+            nome : nome,
             email : login,
             senha : senha
         }
 
-        fetch(`${urlBase}/usuarios/login`, {
+        fetch(`${urlBase}/usuarios`, {
             method: 'POST',
             headers: {'Content-type' : "application/json"},
             body: JSON.stringify(dadosLogin)
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data.errors)
-            if(data.access_token){
-                localStorage.setItem('token', data.access_token)
-                window.location.href = 'municipios.html'
-                document.getElementById('mensagem').innerHTML = `Tudo certo`
+            if(data){
+                window.location.href = 'index.html'
+                document.getElementById('mensagem').innerHTML = `Usuário criado com sucesso`
             } else if(data.errors) {
                 const errorMessages = data.errors.map(error => error.msg).join('<br>')
                 document.getElementById('mensagem').innerHTML = `<span class='text-danger'>${errorMessages}</span>`
